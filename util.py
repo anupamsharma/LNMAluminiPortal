5b43963842fa3cbc6d7be332a3389e11dfc5ccf4
anupamsharma <anu.lnmiit.1985@gmail.com>
2013-01-05 00:19:08 +0530

    Update util.py
    Adding methods to connect and get data.

--- util.py
+++ util.py
@@ -1,30 +1,40 @@
-import MySQLdb
+"""
+This module has utility methods we need for this small application.
+
+"""
+import MySQLdb as MDB
 import json
 
 class DBUtil(object):
   """
-  This class has basic database utility method 
-  """
+  This class has basic database utility method.
   
+  """
+  # We need to use the same names in database configuration file.
+  HOST = 'host'
+  USER = 'user'
+  PASSWD = 'passwd'
+  DB = 'db'
+  
+  def __init__(self):
+    pass
+  
+  @staticmethod
   def get_connection(connection_name, config_file_loc):
     assert(isinstance(connection_name, str))
     assert(isinstance(config_file_loc, str))
     config_file = open(config_file)
     data = json.load(config_file)
     assert(connection_name in data)
-  
-  db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="john", # your username
-                     passwd="megajonhy", # your password
-                     db="jonhydb") # name of the data base
+    
+    return MDB.connect(host=data[DBUtil.HOST],
+                           user=data[DBUtil.USER],
+                           passwd=data[DBUtil.PASSWD],
+                           db=data[DBUtil.DB])
 
-# you must create a Cursor object. It will let
-#  you execute all the query you need
-cur = db.cursor() 
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
-
-# print all the first cell of all the rows
-for row in cur.fetchall() :
-    print row[0]
+  @staticmethod
+  def get_result_as_dicts(conn, query_str):
+    cursor = con.cursor(MDB.cursors.DictCursor)
+    cursor.execute(query_str)
+    return cursor.fetchall()
