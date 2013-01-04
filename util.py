da7803d894dc754ebc993b32395e9ecda1daf3da
anupamsharma <anu.lnmiit.1985@gmail.com>
2013-01-05 00:35:56 +0530

    Update util.py
    Adding support to read config file.

--- util.py
+++ util.py
@@ -2,8 +2,35 @@
 This module has utility methods we need for this small application.
 
 """
+import os
 import MySQLdb as MDB
 import json
+
+class ConfigReader(object):
+  """
+  This class has basic database utility method.
+  
+  """
+  # We need to use the same names in database configuration file.
+  ENV = 'env'
+  PROD = 'prod'
+  CONNECTION_NAME = 'connection_name'
+  PORTAL_HOME_DIR = 'portal_home_dir'
+  DEFAULT_CONFIG_FILE = os.path.join(PORTAL_HOME_DIR, 'cfg', 'main_config.json') 
+
+  def __init__(self):
+    pass
+  
+  @staticmethod
+  def get_configuration(config_file_loc=None):
+    assert(isinstance(connection_name, str))
+    if config_file_loc:
+      assert(isinstance(config_file_loc, str))
+    else:
+      config_file_loc = ConfigReader.DEFAULT_CONFIG_FILE
+      
+    config_file = open(config_file)
+    return json.load(config_file)
 
 class DBUtil(object):
   """
@@ -15,14 +42,19 @@ class DBUtil(object):
   USER = 'user'
   PASSWD = 'passwd'
   DB = 'db'
-  
+  DEFAULT_CONFIG_FILE = os.path.join(PORTAL_HOME_DIR, 'cfg', 'db_config.json') 
+
   def __init__(self):
     pass
   
   @staticmethod
   def get_connection(connection_name, config_file_loc):
     assert(isinstance(connection_name, str))
-    assert(isinstance(config_file_loc, str))
+    if config_file_loc:
+      assert(isinstance(config_file_loc, str))
+    else:
+      config_file_loc = DBUtil.DEFAULT_CONFIG_FILE
+      
     config_file = open(config_file)
     data = json.load(config_file)
     assert(connection_name in data)
@@ -37,4 +69,4 @@ class DBUtil(object):
   def get_result_as_dicts(conn, query_str):
     cursor = con.cursor(MDB.cursors.DictCursor)
     cursor.execute(query_str)
-    return cursor.fetchall()
+    return cursor.fetchall()    
