959230130d6c7b52d3d8310f3e18573d09ba3a35
anupamsharma <anu.lnmiit.1985@gmail.com>
2013-01-05 23:22:53 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,6 +1,31 @@
 from util import ConfigReader, DBUtil
 
-class User():
+
+class Entity(object):
+    def __init__(self):
+        pass
+    
+    def get_attributes(self):
+        raise NotImplentedError
+    
+    def set_attributes(self):
+        raise NotImplentedError
+    
+    def get_id():
+
+class EntitySync(Entity):
+    def __init__(self):
+        pass
+    
+    def get_attributes(self):
+        raise NotImplentedError
+    
+    def set_attributes(self):
+        raise NotImplentedError
+    
+    def get_id():
+
+class User(Entity):
     """
     This class represents a user.
 
