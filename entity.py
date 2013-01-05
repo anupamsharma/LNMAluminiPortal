95c48c945d370f05d0a546c9d0009c0450900cb3
anupamsharma <anu.lnmiit.1985@gmail.com>
2013-01-05 23:25:30 +0530

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,1 +1,30 @@
-entities
+"""
+This module has 
+1) Base Entity class
+2)
+3)
+
+"""
+class Entity(object):
+    def __init__(self):
+        pass
+    
+    def get_attributes(self):
+        raise NotImplentedError
+    
+    def set_attributes(self):
+        raise NotImplentedError
+    
+    def get_id():
+
+class EntitySync(Entity):
+    def __init__(self):
+        pass
+    
+    def get_attributes(self):
+        raise NotImplentedError
+    
+    def set_attributes(self):
+        raise NotImplentedError
+    
+    def get_id():
